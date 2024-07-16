46d81185468478c1647ba023b4a07f387c4eb100
Amirulhafiz <ahmad.amirulhafiz@gmail.com>
2024-07-16 22:51:57 +0800

    First commit, initialize folders and packages

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: Center(child: Text('GitHub Repo')),));
+  }
+}
